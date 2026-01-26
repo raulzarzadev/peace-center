a648863fb34bedcd2da41c435cdcfa429365985b
Raul Zarza <raulzarza.dev@gmail.com>
2026-01-26 10:40:21 -0700

    refactor: Consistent use of double quotes and improve code formatting across multiple components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	i18n: {
+		defaultLocale: "es",
+		locales: ["es", "en"],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
